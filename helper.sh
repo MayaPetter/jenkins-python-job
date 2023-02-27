3f11051ca24d10fcceaa31777d4c3408d79c340c
MayaPetter <mayapetter@gmail.com>
2023-02-27 14:47:37 +0200

    Fix typos in README And fix sed

--- helper.sh
+++ helper.sh
@@ -8,5 +8,5 @@ new_string="${REPO_URL}"
 
 # loop through all the XML files in the directory and replace the string
 for file in $dir_path/*/config.xml; do
-    sed -i 's/${old_string}/${new_string}/g' "$file"
+    sed -i 's+${old_string}+${new_string}+g' "$file"
 done
